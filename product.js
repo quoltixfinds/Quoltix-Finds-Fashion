aad4d55ee67693788a84271539df1287223a2596
Quoltix Finds <quoltixfinds@gmail.com>
2026-05-14 19:28:32 +0530

    Update product.js

--- product.js
+++ product.js
@@ -40,7 +40,7 @@ const products = [
     gender: "female",
     affiliateLink: "https://affiliate.meesho.com/collection/MzMxMTY3MTo6Ojo6Om5vcm1hbA=="
   },
-    {
+  {
     id: 5,
     name: "Striped Maxi Dress",
     image: "Media/Products/QF-FS-5.jpeg",
@@ -50,7 +50,7 @@ const products = [
     gender: "female",
     affiliateLink: "https://affiliate.meesho.com/collection/MzM3NjAxNzo6Ojo6Om5vcm1hbA=="
   },
-      {
+  {
     id: 6,
     name: "Denim A-Line Dress",
     image: "Media/Products/QF-FS-6.jpeg",
@@ -60,65 +60,14 @@ const products = [
     gender: "female",
     affiliateLink: "https://affiliate.meesho.com/collection/MzM5Mjk3MTo6Ojo6Om5vcm1hbA=="
   },
-
-  // {
-  //   id: 4,
-  //   name: "Yellow Summer Dress",
-  //   image: "https://via.placeholder.com/300x400?text=Yellow+Dress",
-  //   category: "shirts",
-  //   tag: "trending",
-  //   sku: "QF104",
-  //   gender: "female",
-  //   affiliateLink: "https://example.com/affiliate4"
-  // },
-  // {
-  //   id: 5,
-  //   name: "Silver Earrings Set",
-  //   image: "Media/Products/QF-FS-4.png",
-  //   category: "Accessories",
-  //   tag: "under999",
-  //   sku: "QF-FS-4",
-  //   gender: "unisex",
-  //   affiliateLink: "https://example.com/affiliate5"
-  // },
-  // {
-  //   id: 6,
-  //   name: "Tote Bag with Strap",
-  //   image: "Media/Products/QF-FS-5.png",
-  //   category: "Bags",
-  //   tag: "best",
-  //   sku: "QF-FS-5",
-  //   gender: "male",
-  //   affiliateLink: "https://example.com/affiliate6"
-  // },
-  // {
-  //   id: 7,
-  //   name: "Black Striped Dress",
-  //   image: "Media/Products/QF-FS-6.png",
-  //   category: "Dress",
-  //   tag: "best",
-  //   sku: "QF-FS-6",
-  //   gender: "female",
-  //   affiliateLink: "https://example.com/affiliate7"
-  // },
-  // {
-  //   id: 8,
-  //   name: "Diamond Pendant",
-  //   image: "Media/Products/QF-FS-7.png",
-  //   category: "Jewelry",
-  //   tag: "trending",
-  //   sku: "QF-FS-7",
-  //   gender: "unisex",
-  //   affiliateLink: "https://example.com/affiliate8"
-  // },
-  // {
-  //   id: 9,
-  //   name: "Canvas Backpack",
-  //   image: "Media/Products/QF-FS-8.png",
-  //   category: "Bags",
-  //   tag: "under999",
-  //   sku: "QF-FS-8",
-  //   gender: "male",
-  //   affiliateLink: "https://example.com/affiliate9"
-  // },
+  {
+    id: 7,
+    name: "Denim A-Line Dress",
+    image: "Media/Products/QF-FS-7.jpeg",
+    category: "Couple Wear",
+    tag: "couple",
+    sku: "QF-FS-7",
+    gender: "unisex",
+    affiliateLink: "https://affiliate.meesho.com/collection/MzU0NzA4MTo6Ojo6Om5vcm1hbA=="
+  },
 ];
