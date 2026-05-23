124f6aead7443484066cb3bb8a427586db5f7fe3
Quoltix Finds <quoltixfinds@gmail.com>
2026-05-23 16:53:22 +0530

    Update product.js

--- product.js
+++ product.js
@@ -10,6 +10,7 @@ const products = [
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3086453?p_id=669339639&ext_id=b2i9rr&utm_source=instagram_stories"
   },
+
   {
     id: 2,
     name: "Striped Maxi Dress",
@@ -20,82 +21,90 @@ const products = [
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3086484?p_id=591020413&ext_id=9rvm9p&utm_source=instagram_stories"
   },
+
   {
     id: 3,
     name: "Yellow Midi Dress",
     image: "Media/Products/QF-FS-3.jpeg",
     category: "Dress",
-    tag: "trending",
+    tag: "ethnic",
     sku: "QF-FS-3",
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3086511?p_id=636640326&ext_id=aj1eti&utm_source=instagram_stories"
   },
+
   {
     id: 4,
     name: "Bodycon Midi Dress",
     image: "Media/Products/QF-FS-4.jpeg",
     category: "Dress",
-    tag: "trending",
+    tag: "party",
     sku: "QF-FS-4",
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3311671?p_id=531357483&ext_id=8scu23&utm_source=instagram_stories"
   },
+
   {
     id: 5,
     name: "Striped Maxi Dress",
     image: "Media/Products/QF-FS-5.jpeg",
     category: "Dress",
-    tag: "trending",
+    tag: "casual",
     sku: "QF-FS-5",
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3376017?p_id=600644779&ext_id=9xlwh7&utm_source=instagram_stories"
   },
+
   {
     id: 6,
     name: "Denim A-Line Dress",
     image: "Media/Products/QF-FS-6.jpeg",
     category: "Dress",
-    tag: "trending",
+    tag: "casual",
     sku: "QF-FS-6",
     gender: "female",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3392971?p_id=497633728&ext_id=88a0n4&utm_source=instagram_stories"
   },
+
   {
     id: 7,
     name: "Traditional Wedding Outfit",
     image: "Media/Products/QF-FS-7.jpeg",
     category: "Couple Wear",
-    tag: "couple",
+    tag: "wedding",
     sku: "QF-FS-7",
     gender: "unisex",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3547081?p_id=438736743&ext_id=797nfr&utm_source=instagram_stories"
   },
+
   {
     id: 8,
     name: "Ethnic Couple Outfit",
     image: "Media/Products/QF-FS-8.jpeg",
     category: "Couple Wear",
-    tag: "couple",
+    tag: "ethnic",
     sku: "QF-FS-8",
     gender: "unisex",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3621734?p_id=340819245&ext_id=5mwxt9&utm_source=instagram_stories"
   },
+
   {
     id: 9,
     name: "Casual Couple Sweatshirt",
     image: "Media/Products/QF-FS-9.jpeg",
-    category: "Couple Wear",
-    tag: "couple",
+    category: "Sweatshirt",
+    tag: "casual",
     sku: "QF-FS-9",
     gender: "unisex",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3621887?p_id=520510821&ext_id=8lwcpx&utm_source=instagram_stories"
   },
+
   {
     id: 10,
     name: "Blue Formal Outfit",
     image: "Media/Products/QF-FS-10.jpeg",
-    category: "Men Fashion",
-    tag: "trending",
+    category: "Shirt",
+    tag: "formal",
     sku: "QF-FS-10",
     gender: "male",
     affiliateLink: "https://www.meesho.com/s/p/a39bi2?product_id=610135418&af_force_deeplink=true&host_internal=single_product&pid=meesho_affiliate_portal&is_retargeting=true&af_click_lookback=7d&product_name=product&utm_source=instagram_stories&external_product_id=a39bi2&af_reengagement_window=14d&c=216131561:instagram_stories:3732315"
@@ -105,18 +114,19 @@ const products = [
     id: 11,
     name: "Grey Cargo Joggers",
     image: "Media/Products/QF-FS-11.jpeg",
-    category: "Men Fashion",
-    tag: "trending",
+    category: "Cargo",
+    tag: "streetwear",
     sku: "QF-FS-11",
     gender: "male",
     affiliateLink: "https://www.meesho.com/s/p/7zbh7d?product_id=482585305&af_force_deeplink=true&host_internal=single_product&pid=meesho_affiliate_portal&is_retargeting=true&af_click_lookback=7d&product_name=product&utm_source=instagram_stories&external_product_id=7zbh7d&af_reengagement_window=14d&c=216131561:instagram_stories:3732333"
   },
+
   {
     id: 12,
     name: "White Casual Kurta",
     image: "Media/Products/QF-FS-12.jpeg",
-    category: "Men Fashion",
-    tag: "trending",
+    category: "Kurta",
+    tag: "ethnic",
     sku: "QF-FS-12",
     gender: "male",
     affiliateLink: "https://www.meesho.com/af_invite/216131561:instagram_stories:3788930?p_id=100479226&ext_id=1ntm9m&utm_source=instagram_stories"
